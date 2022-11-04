a7eb23b8c7b9ab09b7d10cde2c2bce7d9ba6e44b
wrenfannin <luwr5102@gmail.com>
2022-11-04 10:03:40 +0000

    formatting && ascii

--- Iterations/ManyButtonsToManyText.ino/ManyButtonsToManyText/ManyButtonsToManyText.ino
+++ Iterations/ManyButtonsToManyText.ino/ManyButtonsToManyText/ManyButtonsToManyText.ino
@@ -1,15 +1,24 @@
+/***
+ *       _____             _____                          _        
+ *      / ____|           |  __ \                        | |       
+ *     | |      ___ __  __| |__) | ___  _ __ ___    ___  | |_  ___ 
+ *     | |     / _ \\ \/ /|  _  / / _ \| '_ ` _ \  / _ \ | __|/ _ \
+ *     | |____| (_) |>  < | | \ \|  __/| | | | | || (_) || |_|  __/
+ *      \_____|\___//_/\_\|_|  \_\\___||_| |_| |_| \___/  \__|\___|
+ *                                                                 
+ *                                                                 
+ */
+
 #define REMOTEXY_MODE__ESP32CORE_BLE
 #include <BLEDevice.h>
 #include <TFT_eSPI.h>
 #include "Free_Fonts.h"
 #include <RemoteXY.h>
 #include <SPI.h>
-
-// RemoteXY connection settings 
 #define REMOTEXY_BLUETOOTH_NAME "CoxRemote"
+
 TFT_eSPI tft = TFT_eSPI();
 
-// RemoteXY configurate  
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =   // 118 bytes
   { 255,6,0,0,0,111,0,16,31,1,1,0,2,5,19,19,37,24,83,104,
@@ -19,10 +28,8 @@ uint8_t RemoteXY_CONF[] =   // 118 bytes
   116,0,1,0,4,62,20,20,193,24,83,104,111,119,32,84,101,120,116,0,
   1,0,35,62,21,21,124,24,83,104,111,119,32,84,101,120,116,0 };
   
-// this structure defines all the variables and events of your control interface 
 struct {
 
-    // input variables
   uint8_t button_0; // =1 if button pressed, else =0 
   uint8_t button_1; // =1 if button pressed, else =0 
   uint8_t button_2; // =1 if button pressed, else =0 
@@ -30,7 +37,6 @@ struct {
   uint8_t button_4; // =1 if button pressed, else =0 
   uint8_t button_5; // =1 if button pressed, else =0 
 
-    // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0 
 
 } RemoteXY;
@@ -42,8 +48,6 @@ void setup()
   tft.init();
   tft.fillScreen(TFT_RED);
   tft.setFreeFont(&FreeSansBoldOblique24pt7b);
-  
-  // TODO you setup code
   
 }
 
@@ -69,10 +73,4 @@ void loop()
   if (RemoteXY.button_5 == 1){
     tft.drawString("Text Six",10,120,4);
   }
-
-  // TODO you loop code
-  // use the RemoteXY structure for data transfer
-  // do not call delay() 
-
-
 }
